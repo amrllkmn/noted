dca1425c4b25cc23f0246d4e13e9c048dc169e12
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-11-19 18:25:51 +0800

    Add basic tests

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,6 @@
 use axum::{extract::Path, http::StatusCode, Json};
 use serde_json::{json, Value};
-use crate::model::Note;
+use crate::model;
 
 pub async fn healthcheck() -> (StatusCode, Json<Value>) {
     let resp = Json(json!({"data": "OK"}));
@@ -8,14 +8,16 @@ pub async fn healthcheck() -> (StatusCode, Json<Value>) {
 }
 
 pub async fn get_notes_list() -> (StatusCode, Json<Value>) {
-    let sample_note = Note {
-        id: 1,
-        title: "Test".to_string(),
-        content: "Hello World!".to_string()
-    };
-    (StatusCode::OK, Json(json!({"data": [
-        sample_note
-    ]})))
+    
+    if let Ok(sample_note) = model::get_one_note().await {
+        
+        (StatusCode::OK, Json(json!({"data": [
+            sample_note
+        ]})))
+    } else {
+        (StatusCode::NOT_FOUND, Json(json!({
+        "data": "Note not found"})))
+    }
 }
 
 pub async fn post_note() -> (StatusCode, Json<Value>) {
@@ -33,3 +35,4 @@ pub async fn update_note(Path(id): Path<String>) -> (StatusCode, Json<Value>) {
 pub async fn delete_note(Path(id): Path<String>) -> (StatusCode, Json<Value>) {
     todo!()
 }
+
--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,4 @@
+use axum::Error;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug,Deserialize, Serialize)]
@@ -5,4 +6,34 @@ pub struct Note {
     pub id: u32,
     pub title: String,
     pub content: String
+}
+
+pub async fn get_one_note() -> Result<Note, Error> {
+
+    let sample_note = Note {
+        id: 1,
+        title: "Test".to_string(),
+        content: "Hello World!".to_string()
+    };
+
+    Ok(sample_note)
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::get_one_note;
+
+
+    #[tokio::test]
+    async fn test_should_pass() {
+        if let Ok(note)  =  get_one_note().await {
+            assert_eq!(note.content, "Hello World!".to_string());    
+        }
+    }
+
+    // #[tokio::test]
+    // async fn test_should_fail() {
+        // assert_eq!(1,2);
+    // }
 }
